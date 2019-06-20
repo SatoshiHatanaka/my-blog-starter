812cb65591d6a6bd7ed75b3a8cf8086bd7e111bf
Satoshi Hatanaka <developer.satoshi.hatanaka@gmail.com>
2019-06-20 14:33:51 +0900

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Blog`,
+    author: `Satoshi Hatanaka`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `PastelCruller`,
     },
   },
   plugins: [
